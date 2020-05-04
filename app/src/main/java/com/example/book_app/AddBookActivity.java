bf038d51ba8cb6ab5385c81831785f02f85a628d
Ian Taylor <it1610@hsutx.edu>
2020-05-04 18:24:41 -0500

    Implemented Activity for accepting user information and saving to database, then displaying in recyclerview. //Partially works, crashes if you save without entering data a few times in a row.

---
+++ app/src/main/java/com/example/book_app/AddBookActivity.java
@@ -0,0 +1,102 @@
+package com.example.book_app;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.NumberPicker;
+import android.widget.Spinner;
+import android.widget.Toast;
+
+public class AddBookActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
+    public static final String EXTRA_TITLE =
+            "com.example.book_app.EXTRA_TITLE";
+    public static final String EXTRA_AUTHOR =
+            "com.example.book_app.EXTRA_AUTHOR";
+    public static final String EXTRA_PAGES =
+            "com.example.book_app.EXTRA_PAGES";
+    public static final String EXTRA_STATUS =
+            "com.example.book_app.EXTRA_STATUS";
+
+
+    private EditText editTextTitle;
+    private EditText editTextAuthor;
+    private EditText editTextPage;
+    private Spinner spinnerStatus;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_book);
+
+        editTextTitle = findViewById(R.id.edit_text_title);
+        editTextAuthor = findViewById(R.id.edit_text_author);
+        editTextPage = findViewById(R.id.edit_text_pages);
+
+        spinnerStatus = findViewById(R.id.spinner_status);
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this, R.array.stuses, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinnerStatus.setAdapter(adapter);
+        spinnerStatus.setOnItemSelectedListener(this);
+
+        getSupportActionBar().setHomeAsUpIndicator(R.drawable.ic_close);
+        setTitle("Add Book");
+    }
+
+    private void saveBook() {
+        String title = editTextTitle.getText().toString();
+        String author = editTextAuthor.getText().toString();
+        int pages = Integer.parseInt(editTextPage.getText().toString());
+        String status = spinnerStatus.getSelectedItem().toString();
+
+        if (title.trim().isEmpty() || author.trim().isEmpty()) {
+            Toast.makeText(this, "Please fill out all the elements", Toast.LENGTH_SHORT);
+            return;
+        }
+
+        Intent data = new Intent();
+        data.putExtra(EXTRA_TITLE, title);
+        data.putExtra(EXTRA_AUTHOR, author);
+        data.putExtra(EXTRA_PAGES, pages);
+        data.putExtra(EXTRA_STATUS, status);
+
+        setResult(RESULT_OK, data);
+        finish();
+
+    }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater menuInflater = getMenuInflater();
+        menuInflater.inflate(R.menu.add_book_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.save_book:
+                saveBook();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
+    @Override
+    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+        String text = parent.getItemAtPosition(position).toString();
+        Toast.makeText(parent.getContext(), text, Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> parent) {
+
+    }
+}
--- app/src/main/java/com/example/book_app/MainActivity.java
+++ app/src/main/java/com/example/book_app/MainActivity.java
@@ -6,18 +6,32 @@ import androidx.lifecycle.ViewModelProvider;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Toast;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+public static final int ADD_BOOK_REQUEST = 1;
     private BookViewModel bookViewModel;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        FloatingActionButton buttonAddBook = findViewById(R.id.button_add_book);
+        buttonAddBook.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, AddBookActivity.class);
+                startActivityForResult(intent,ADD_BOOK_REQUEST);
+            }
+        });
 
         RecyclerView recyclerView = findViewById(R.id.recycler_view);
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
@@ -38,4 +52,24 @@ public class MainActivity extends AppCompatActivity {
             }
         });
     }
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode==ADD_BOOK_REQUEST && resultCode==RESULT_OK) {
+            String title = data.getStringExtra(AddBookActivity.EXTRA_TITLE);
+            String author = data.getStringExtra(AddBookActivity.EXTRA_AUTHOR);
+            String status = data.getStringExtra(AddBookActivity.EXTRA_STATUS);
+            int pages = data.getIntExtra(AddBookActivity.EXTRA_PAGES, 0);
+
+            Book book = new Book(title, author, status, pages);
+            bookViewModel.insert(book);
+
+            Toast.makeText(this, "Book Saved", Toast.LENGTH_SHORT).show();
+        } else {
+            Toast.makeText(this, "Failed to save!", Toast.LENGTH_SHORT).show();
+        }
+
+    }
+
 }
