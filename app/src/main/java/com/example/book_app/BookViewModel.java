96290f0581fa14d2c31903a82313e84d4d19c79f
Justin <jkt1609@hsutx.edu>
2020-05-04 03:22:45 -0500

    Implemented the BookViewModel. App is running now with correct toast message!

--- app/src/main/java/com/example/book_app/BookViewModel.java
+++ app/src/main/java/com/example/book_app/BookViewModel.java
@@ -2,29 +2,35 @@ package com.example.book_app;
 
 import android.app.Application;
 
-import java.util.List;
-
 import androidx.annotation.NonNull;
 import androidx.lifecycle.AndroidViewModel;
 import androidx.lifecycle.LiveData;
 
+import java.util.List;
+
 public class BookViewModel extends AndroidViewModel {
     private BookRepository repository;
     private LiveData<List<Book>> allBooks;
+
     public BookViewModel(@NonNull Application application) {
         super(application);
+        repository = new BookRepository(application);
+        allBooks = repository.getAllBooks();
     }
 
-    public void insert(Book book) {
+    public void insert(Book book){
         repository.insert(book);
     }
-    public void update(Book book) {
+
+    public void update(Book book){
         repository.update(book);
     }
-    public void delete(Book book) {
+
+    public void delete(Book book){
         repository.delete(book);
     }
-    public void deleteAllBooks() {
+
+    public void deleteAllBooks(){
         repository.deleteAllBooks();
     }
 
--- app/src/main/java/com/example/book_app/MainActivity.java
+++ app/src/main/java/com/example/book_app/MainActivity.java
@@ -1,14 +1,32 @@
 package com.example.book_app;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProvider;
 
 import android.os.Bundle;
+import android.widget.Toast;
+
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+    private BookViewModel bookViewModel;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        bookViewModel = new ViewModelProvider(this
+                , ViewModelProvider.AndroidViewModelFactory.getInstance(this.getApplication()))
+                .get(BookViewModel.class);
+
+        bookViewModel.getAllBooks().observe(this, new Observer<List<Book>>() {
+            @Override
+            public void onChanged(List<Book> notes) {
+
+                Toast.makeText(MainActivity.this, "Onchange ", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
